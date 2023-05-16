32a58c979c7eb30f8de48a4b7e4f2e8f290d18a8
LDom890 <martin.velazquez.luis.alfonso@gmail.com>
2023-05-16 11:04:54 -0600

    GravityScene to sandbox

---
+++ Assets/CGMWA_VR/Code/Scripts/GravityEntity.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityEntity : MonoBehaviour
+{
+    public const float GravityScale = 9.81f;
+
+    [SerializeField] private Rigidbody _rb;
+    
+    private Vector3 _gravityDir = Vector3.down;
+
+    private void Awake()
+    {
+        if (!_rb) _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(_rb.mass * GravityScale * _gravityDir);
+    }
+
+    [Button]
+    public void ChangeGravity(Vector3 dir)
+    {
+        _gravityDir = dir;
+    }
+}
